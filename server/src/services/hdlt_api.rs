603684ee9ef0620d0d694b4179ae7666b1a5fd4c
André Breda <andrebreda@tecnico.ulisboa.pt>
2021-04-15 15:43:32 +0100

    cargo fmt

--- server/src/services/hdlt_api.rs
+++ server/src/services/hdlt_api.rs
@@ -263,7 +263,10 @@ mod test {
         );
 
         // sometimes there's nothing to see
-        assert!(SVC.users_at_position(ha_client_id, Position(123, 123), 67981463).unwrap().is_empty());
+        assert!(SVC
+            .users_at_position(ha_client_id, Position(123, 123), 67981463)
+            .unwrap()
+            .is_empty());
     }
 
     #[test]

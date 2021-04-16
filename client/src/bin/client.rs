655e6f0d94fcdbe5889aff55caf42014b1f3a6f3
André Breda <andrebreda@tecnico.ulisboa.pt>
2021-04-16 02:58:23 +0100

    Refactor client for easier integration testing

--- client/src/bin/client.rs
+++ client/src/bin/client.rs
@@ -1,48 +1,6 @@
-use std::{path::PathBuf, sync::Arc};
-
-use model::keys::KeyStore;
+use client::{Client, Options};
 use structopt::StructOpt;
-use tonic::transport::Uri;
 use tracing::*;
-
-use client::driver::DriverService;
-use client::hdlt_api::HdltApiClient;
-use client::malicious_driver::MaliciousDriverService;
-use client::malicious_witness::MaliciousWitnessService;
-use client::state::{CorrectClientState, MaliciousClientState};
-use client::witness::WitnessService;
-use protos::driver::driver_server::DriverServer;
-use protos::driver::malicious_driver_server::MaliciousDriverServer;
-use protos::witness::witness_server::WitnessServer;
-use tonic::transport::Server;
-
-use tokio::sync::RwLock;
-
-#[derive(StructOpt)]
-struct Options {
-    /// Server URI
-    #[structopt(short = "s", long = "server")]
-    server_uri: Uri,
-
-    /// Whether the client is malicious
-    #[structopt(short, long)]
-    malicious: bool,
-
-    /// Bind address
-    bind_addr: std::net::SocketAddr,
-
-    /// path to entity registry
-    ///
-    /// See [KeyStore] for more information.
-    #[structopt(short = "e", long = "entities", env = "ENTITY_REGISTRY_PATH")]
-    entity_registry_path: PathBuf,
-
-    /// path to client secret keys
-    ///
-    /// See [KeyStore] for more information.
-    #[structopt(short = "k", long = "secret-keys", env = "SECRET_KEYS_PATH")]
-    skeys_path: PathBuf,
-}
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
@@ -50,104 +8,20 @@ async fn main() -> eyre::Result<()> {
     color_eyre::install()?;
     // do not remove
     let _guard = tracing_utils::setup(env!("CARGO_PKG_NAME"))?;
-    true_main().await
+
+    let options = Options::from_args();
+    let (client, _task_handle) = Client::new(&options)?;
+
+    true_main(&client).await
 }
 
 #[instrument]
-async fn true_main() -> eyre::Result<()> {
-    let options = Options::from_args();
+async fn true_main(client: &Client) -> eyre::Result<()> {
+    let reply = client.obtain_position_report(0, 0).await?;
+    info!(
+        event = "We asked the server to do the thing and got a reply",
+        ?reply
+    );
 
-    let keystore = Arc::new(KeyStore::load_from_files(
-        options.entity_registry_path.clone(),
-        options.skeys_path.clone(),
-    )?);
-
-    let driver_task = async {
-        if options.malicious {
-            malicious_driver_server(
-                options.bind_addr,
-                keystore.clone(),
-                options.server_uri.clone(),
-            )
-            .await
-        } else {
-            driver_server(
-                options.bind_addr,
-                keystore.clone(),
-                options.server_uri.clone(),
-            )
-            .await
-        }
-    };
-
-    let main_task = async {
-        let client = HdltApiClient::new(options.server_uri.clone(), keystore.clone())?;
-        let reply = client.obtain_position_report(0, 0).await?;
-        info!(
-            event = "We asked the server to do the thing and got a reply",
-            ?reply
-        );
-
-        Ok::<_, eyre::Report>(())
-    };
-
-    tokio::select! {
-        Err(e) = driver_task => {
-            error!("Driver aborted: {:#?}", e);
-        }
-        _ = main_task => (),
-    }
-
-    Ok(())
-}
-
-async fn ctrl_c() {
-    use std::future;
-
-    if tokio::signal::ctrl_c().await.is_err() {
-        eprintln!("Failed to listen for Ctrl+C/SIGINT. Server will still exit after receiving them, just not gracefully.");
-        future::pending().await // never completes
-    }
-}
-
-async fn malicious_driver_server(
-    bind_addr: std::net::SocketAddr,
-    keystore: Arc<KeyStore>,
-    server_uri: Uri,
-) -> eyre::Result<()> {
-    let state = Arc::new(RwLock::new(MaliciousClientState::new()));
-    let server = Server::builder()
-        .add_service(MaliciousDriverServer::new(MaliciousDriverService::new(
-            state.clone(),
-            keystore.clone(),
-            server_uri,
-        )))
-        .add_service(WitnessServer::new(MaliciousWitnessService::new(
-            keystore, state,
-        )))
-        .serve_with_shutdown(bind_addr, ctrl_c());
-    info!("Malicious Driver Server @{:?}: listening", bind_addr);
-    server.await?;
-    info!("Malicious Driver Server @{:?}: finished", bind_addr);
-    Ok(())
-}
-
-async fn driver_server(
-    bind_addr: std::net::SocketAddr,
-    keystore: Arc<KeyStore>,
-    server_uri: Uri,
-) -> eyre::Result<()> {
-    let state = Arc::new(RwLock::new(CorrectClientState::new()));
-    let server = Server::builder()
-        .add_service(DriverServer::new(DriverService::new(
-            state.clone(),
-            keystore.clone(),
-            server_uri,
-        )))
-        .add_service(WitnessServer::new(WitnessService::new(keystore, state)))
-        .serve_with_shutdown(bind_addr, ctrl_c());
-    info!("Driver Server @{:?}: listening", bind_addr);
-    server.await?;
-    info!("Driver Server @{:?}: finished", bind_addr);
     Ok(())
 }
--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,7 +1,171 @@
-pub mod driver;
-pub mod hdlt_api;
-pub mod malicious_driver;
-pub mod malicious_witness;
-pub mod state;
-pub mod witness;
+pub(crate) mod driver;
+pub(crate) mod hdlt_api;
+pub(crate) mod malicious_driver;
+pub(crate) mod malicious_witness;
+pub(crate) mod state;
+pub(crate) mod witness;
 mod witness_api;
+
+use std::path::PathBuf;
+use std::sync::Arc;
+use std::{net::SocketAddr, ops::Deref};
+
+use structopt::StructOpt;
+use tokio::sync::RwLock;
+use tokio_stream::wrappers::TcpListenerStream;
+use tonic::transport::{Server, Uri};
+use tracing::info;
+
+use model::keys::KeyStore;
+use protos::driver::driver_server::DriverServer;
+use protos::driver::malicious_driver_server::MaliciousDriverServer;
+use protos::witness::witness_server::WitnessServer;
+
+use driver::DriverService;
+use hdlt_api::HdltApiClient;
+use malicious_driver::MaliciousDriverService;
+use malicious_witness::MaliciousWitnessService;
+use state::{CorrectClientState, MaliciousClientState};
+use witness::WitnessService;
+
+#[derive(StructOpt, Debug)]
+pub struct Options {
+    /// Server URI
+    #[structopt(short = "s", long = "server")]
+    pub server_uri: Uri,
+
+    /// Whether the client is malicious
+    #[structopt(short, long)]
+    pub malicious: bool,
+
+    /// Bind address
+    pub bind_addr: std::net::SocketAddr,
+
+    /// path to entity registry
+    ///
+    /// See [KeyStore] for more information.
+    #[structopt(short = "e", long = "entities", env = "ENTITY_REGISTRY_PATH")]
+    pub entity_registry_path: PathBuf,
+
+    /// path to client secret keys
+    ///
+    /// See [KeyStore] for more information.
+    #[structopt(short = "k", long = "secret-keys", env = "SECRET_KEYS_PATH")]
+    pub skeys_path: PathBuf,
+}
+
+#[derive(Debug)]
+pub struct Client {
+    listen_addr: SocketAddr,
+    api_client: HdltApiClient,
+}
+
+pub type ClientBgTaskHandle = tokio::task::JoinHandle<eyre::Result<()>>;
+impl Client {
+    pub fn new(options: &Options) -> eyre::Result<(Self, ClientBgTaskHandle)> {
+        let keystore = Arc::new(KeyStore::load_from_files(
+            options.entity_registry_path.clone(),
+            options.skeys_path.clone(),
+        )?);
+
+        let (incoming, listen_addr) = create_tcp_incoming(&options.bind_addr)?;
+
+        let is_malicious = options.malicious;
+        let la = listen_addr.clone();
+        let ks = Arc::clone(&keystore);
+        let su = options.server_uri.clone();
+        let client_bg_task = tokio::spawn(async move {
+            if is_malicious {
+                malicious_driver_server(incoming, la, ks, su).await
+            } else {
+                driver_server(incoming, la, ks, su).await
+            }
+        });
+
+        let api_client = HdltApiClient::new(options.server_uri.clone(), keystore)?;
+
+        let client = Client {
+            listen_addr,
+            api_client,
+        };
+        Ok((client, client_bg_task))
+    }
+
+    pub fn api_client(&self) -> &HdltApiClient {
+        &self.api_client
+    }
+
+    pub fn listen_addr(&self) -> &SocketAddr {
+        &self.listen_addr
+    }
+}
+
+impl Deref for Client {
+    type Target = HdltApiClient;
+
+    fn deref(&self) -> &Self::Target {
+        &self.api_client
+    }
+}
+
+async fn ctrl_c() {
+    use std::future;
+
+    if tokio::signal::ctrl_c().await.is_err() {
+        eprintln!("Failed to listen for Ctrl+C/SIGINT. Server will still exit after receiving them, just not gracefully.");
+        future::pending().await // never completes
+    }
+}
+
+async fn malicious_driver_server(
+    incoming: TcpListenerStream,
+    listen_addr: SocketAddr,
+    keystore: Arc<KeyStore>,
+    server_uri: Uri,
+) -> eyre::Result<()> {
+    let state = Arc::new(RwLock::new(MaliciousClientState::new()));
+    let server = Server::builder()
+        .add_service(MaliciousDriverServer::new(MaliciousDriverService::new(
+            state.clone(),
+            Arc::clone(&keystore),
+            server_uri,
+        )))
+        .add_service(WitnessServer::new(MaliciousWitnessService::new(
+            keystore, state,
+        )))
+        .serve_with_incoming_shutdown(incoming, ctrl_c());
+
+    info!("Malicious Driver Server @{:?}: listening", listen_addr);
+    server.await?;
+    info!("Malicious Driver Server @{:?}: finished", listen_addr);
+    Ok(())
+}
+
+async fn driver_server(
+    incoming: TcpListenerStream,
+    listen_addr: SocketAddr,
+    keystore: Arc<KeyStore>,
+    server_uri: Uri,
+) -> eyre::Result<()> {
+    let state = Arc::new(RwLock::new(CorrectClientState::new()));
+    let server = Server::builder()
+        .add_service(DriverServer::new(DriverService::new(
+            state.clone(),
+            Arc::clone(&keystore),
+            server_uri,
+        )))
+        .add_service(WitnessServer::new(WitnessService::new(keystore, state)))
+        .serve_with_incoming_shutdown(incoming, ctrl_c());
+    info!("Driver Server @{:?}: listening", listen_addr);
+    server.await?;
+    info!("Driver Server @{:?}: finished", listen_addr);
+    Ok(())
+}
+
+fn create_tcp_incoming(bind_addr: &SocketAddr) -> eyre::Result<(TcpListenerStream, SocketAddr)> {
+    let listener = std::net::TcpListener::bind(bind_addr)?;
+    let listen_addr = listener.local_addr()?;
+
+    let listener = tokio::net::TcpListener::from_std(listener)?;
+    Ok((TcpListenerStream::new(listener), listen_addr))
+}

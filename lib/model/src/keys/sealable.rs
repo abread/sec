228a47fbcb17017001ebf28c8d270df543c4f53a
André Breda <andrebreda@tecnico.ulisboa.pt>
2021-05-21 01:00:41 +0100

    Sealable: Document and add proper errors

--- lib/model/src/keys/sealable.rs
+++ lib/model/src/keys/sealable.rs
@@ -1,9 +1,18 @@
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use sodiumoxide::crypto::pwhash::scryptsalsa208sha256 as pwhash;
 use sodiumoxide::crypto::secretbox::xsalsa20poly1305 as secretbox;
+use thiserror::Error;
 
 use crate::base64_serialization::Base64SerializationExt;
 
+/// Wrapper struct to protect objects with a password.
+///
+/// It will derive a symmetric key from the password and use it to encrypt the object.
+/// See [sodiumoxide::crypto::pwhash::scryptsalsa208sha256] for information on
+/// the KDF being used, and [sodiumoxide::crypto::secretbox::xsalsa20poly1305]
+/// for information on the symmetric cipher being used.
+/// The salt and nonce required by these two algorithms are generated randomly
+/// when sealing the object.
 #[derive(Serialize, Deserialize, PartialEq, Clone)]
 pub enum Sealable<T> {
     Unsealed(T),
@@ -19,8 +28,24 @@ pub enum Sealable<T> {
     },
 }
 
+#[derive(Debug, Error)]
+pub enum SealableError {
+    #[error("Failed to (de)serialize inner object")]
+    SerializationError(#[from] serde_json::Error),
+
+    #[error("Failed to decrypt object")]
+    DecryptionError,
+
+    #[error("Failed to derive object encryption key")]
+    KeyDerivationError,
+
+    #[error("Object is sealed. Unseal first to be able to perform this operation.")]
+    ObjectSealed,
+}
+
 impl<T> Sealable<T> {
-    pub fn unseal(&mut self, password: &str) -> Result<(), ()>
+    /// Unseal object given the password. No-op if object is already unsealed.
+    pub fn unseal(&mut self, password: &str) -> Result<(), SealableError>
     where
         T: DeserializeOwned,
     {
@@ -37,17 +62,20 @@ impl<T> Sealable<T> {
                 salt,
                 pwhash::OPSLIMIT_INTERACTIVE,
                 pwhash::MEMLIMIT_INTERACTIVE,
-            )?;
+            )
+            .map_err(|_| SealableError::KeyDerivationError)?;
 
-            let plaintext = secretbox::open(&ciphertext, nonce, &key)?;
-            *self = Sealable::Unsealed(serde_json::from_slice(&plaintext).map_err(|_| ())?);
+            let plaintext = secretbox::open(&ciphertext, nonce, &key)
+                .map_err(|_| SealableError::DecryptionError)?;
+            *self = Sealable::Unsealed(serde_json::from_slice(&plaintext)?);
         }
         // even if it was already unselaed, it's fine, just keep going
 
         Ok(())
     }
 
-    pub fn seal(&mut self, password: &str) -> Result<(), ()>
+    /// Seal the (unsealed) object with the given password.
+    pub fn seal(&mut self, password: &str) -> Result<(), SealableError>
     where
         T: Serialize,
     {
@@ -60,10 +88,11 @@ impl<T> Sealable<T> {
                 &salt,
                 pwhash::OPSLIMIT_INTERACTIVE,
                 pwhash::MEMLIMIT_INTERACTIVE,
-            )?;
+            )
+            .map_err(|_| SealableError::KeyDerivationError)?;
 
             let nonce = secretbox::gen_nonce();
-            let plaintext = serde_json::to_vec(&v).map_err(|_| ())?;
+            let plaintext = serde_json::to_vec(&v)?;
             let ciphertext = secretbox::seal(&plaintext, &nonce, &key);
 
             *self = Sealable::Sealed {
@@ -75,10 +104,11 @@ impl<T> Sealable<T> {
             Ok(())
         } else {
             // it's already sealed yes, but with a potentially different password :/
-            Err(())
+            Err(SealableError::ObjectSealed)
         }
     }
 
+    /// Get a reference to the inner object. Panics if object is sealed.
     pub fn get(&self) -> &T {
         if let Sealable::Unsealed(v) = self {
             v
@@ -87,6 +117,7 @@ impl<T> Sealable<T> {
         }
     }
 
+    /// Check if object is sealed.
     pub fn is_sealed(&self) -> bool {
         matches!(self, Sealable::Sealed { .. })
     }
